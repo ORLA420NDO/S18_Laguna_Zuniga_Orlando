3e63cf53cfe1f66dd78730c1cc57607dde34fc2e
ORLA420NDO <92636460+ORLA420NDO@users.noreply.github.com>
2021-11-23 19:08:08 -0600

    Add files via upload

---
+++ Trarea01/T1E2_even_odd.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Digite un numero:"))
+
+
+if num1%2==0:
+  print("El numero es par")
+elif num1%2==0:
+  print(f"{num1} el numero es par")
+
+else:
+  print("El numero es impar")
+
+
---
+++ Trarea01/T1E3_birthday.py
@@ -0,0 +1,6 @@
+
+xx = 28
+xxx = 3
+xxxx = 2001
+
+print(f"Mi cumpleaños es el dia {xx} del mes {xxx} del año {xxxx}")
