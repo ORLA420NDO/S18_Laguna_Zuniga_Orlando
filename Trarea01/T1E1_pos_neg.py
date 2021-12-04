8d4650b08a27b9d5963e7739a2a125a07ba884d5
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-04 01:46:20 -0600

    Update T1E1_pos_neg.py

--- Trarea01/T1E1_pos_neg.py
+++ Trarea01/T1E1_pos_neg.py
@@ -1,12 +1,31 @@
+"""
+Date:       2021-11-23
+Authors:    Orlando Miguel Laguna Zuñiga
+File:       T1E1_pos_neg.py
+Brief:      Programa que pide al usuario un número e indica si este es: 
+            positivo, negativo o igual a cero
+Score:      35
+Version:    0.1.1
+Fixes:      - Falta la condición de __main__
 
-num=int(input("ingresar numero:"))
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores
+            
+            - El programa no funciona cuando el número es cero, si te 
+                atoras mucho puedes apoyarte con tu compañero de equipo 
+                o mandarme whats para organizar un meet extra de la 
+                clase
+"""
 
-if num>=1:
+num=int(input("ingresar numero:"))  # PEP8
+
+if num>=1:                          # PEP8
     print("Numero es positivo")
+
 else:
     print("Numero negativo")
 
-    num=0
-    print("El numero es 0")
-
-
+    # AQUI ESTAS ASIGNANDO DEBERIAS COMPARAR CON EL OPERADOR ==
+    # Y ADEMAS DEBERIA ESTAR ENTRE EL if Y EL else, USANDO elif
+    num=0                           # PEP8
+    print("El numero es 0")         # SIEMPRE IMPRIME CERO SI ES NEGATIVO, ESO ESTA MAL
