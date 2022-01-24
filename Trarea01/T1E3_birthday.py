bfce1cea90f981016a06d5b0094defc5411214c4
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-23 23:01:13 -0600

    Update T1E3_birthday.py

--- Trarea01/T1E3_birthday.py
+++ Trarea01/T1E3_birthday.py
@@ -1,3 +1,17 @@
+"""
+Date:       2021-11-23
+Authors:    Orlando Laguna Zuñiga
+File:       T1E3_birthday.py
+Brief:      Este programa solicita año, mes y día de cumpleaños 
+            e imprime el string 'Year:xxxx - Month:xx - Day: xx'
+            con sus respectivos resultados
+Score:      100
+Versión:    1.1.1
+Fixes:      - Falta la condición de __main__
+
+            * Para este ejercicio me referia ingresar las variables con 
+                input pero en esta ocasion te lo valgo 
+"""
 
 xx = 28
 xxx = 3
