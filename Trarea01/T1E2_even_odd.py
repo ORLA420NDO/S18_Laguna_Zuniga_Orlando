cb4f4b895b4e039e690d3f9cc5474e1e3aa9880b
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-23 22:55:29 -0600

    Update T1E2_even_odd.py

--- Trarea01/T1E2_even_odd.py
+++ Trarea01/T1E2_even_odd.py
@@ -1,3 +1,18 @@
+"""
+Date:       2021-11-23
+Authors:    Orlando Laguna Zuñiga
+File:       T1E2_even_odd.py
+Brief:      Ingresar un número y validar si es par o impar.
+Score:      60+
+Versión:    0.2.1
+Fixes:      - Falta la condición de __main__
+
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores
+            
+            - PEP8 recomienda que la sangría sea un múltiplo de 4 
+                espacio
+"""
 
 num1 = int(input("Digite un numero:"))
 
@@ -9,5 +24,4 @@ elif num1%2==0:
 
 else:
   print("El numero es impar")
-
-
+  
